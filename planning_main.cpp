8659a5fff19843251b85669917b4c874f537de41
YJ <yangjin233@163.com>
2025-04-27 15:57:47 +0800

    version 0.0.1

---
+++ planning_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "process.h"
+using std::cout, std::endl;
+
+int main()
+{
+
+    cout << "planning start" << endl;
+    Process pro;
+    pro.planProcess();
+    cout << "planning end" << endl;
+
+    return 0;
+}
